84c7e6647ece22c1cf2e6dae71203441e7f5d452
lazarljubenovic <lazar.ljubenovic.1995@gmail.com>
2017-09-02 02:34:49 +0200

    feat(log): tokenize the raw log

--- index.js
+++ index.js
@@ -8,10 +8,18 @@ git.log({
   const print = log.all
     .map(entry => entry.message)
     .map(stripBranchInfo)
+    .map(tokenize)
   console.log(print)
 })
 
 function stripBranchInfo(msg) {
   const result = /(.*)\(.+->.+\)$/.exec(msg)
   return result ? result[1].slice(0, -1) : msg
+}
+
+function tokenize(msg) {
+  const result = /^(\S+)\((.*)\):\s?(.*)/g.exec(msg)
+  if (result == null) return {type: null, scope: null, message: msg}
+  const [, type, scope, message] = result
+  return {type, scope, message}
 }
