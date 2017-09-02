fd76661959a844d445769b0fb08eeb286deb3585
lazarljubenovic <lazar.ljubenovic.1995@gmail.com>
2017-09-02 02:33:44 +0200

    feat(options): include only commits in last 16 hours

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const git = require('simple-git')()
 
-git.log({}, (err, log) => {
+git.log({
+  '--all': null,
+  '--since': '16 hours ago',
+}, (err, log) => {
   const print = log.all
     .map(entry => entry.message)
     .map(stripBranchInfo)
