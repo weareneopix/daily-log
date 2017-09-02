f4864342d4c757adfdc8783d2cd44014e339296c
lazarljubenovic <lazar.ljubenovic.1995@gmail.com>
2017-09-02 02:32:12 +0200

    feat(*): strip branch info from commit message

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const git = require('simple-git')
+const git = require('simple-git')()
+
+git.log({}, (err, log) => {
+  const print = log.all
+    .map(entry => entry.message)
+    .map(stripBranchInfo)
+  console.log(print)
+})
+
+function stripBranchInfo(msg) {
+  const result = /(.*)\(.+->.+\)$/.exec(msg)
+  return result ? result[1].slice(0, -1) : msg
+}
